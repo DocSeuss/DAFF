121521a19505540573f0e2801bd4151c844519e7
Cancelo Facundo Ezequiel <facundo.cancelo@gmail.com>
2024-05-09 14:34:38 -0300

    Suba de modificaciones de la registracion

---
+++ Static/js/Usuario.js
@@ -0,0 +1,11 @@
+class Usuario{
+    constructor(id, firstName, lastName, email, birthdate, password, country) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.password = password;
+        this.country = country;        
+    }
+}
